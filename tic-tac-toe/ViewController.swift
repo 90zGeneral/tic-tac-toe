f7a83dd2ff76f83a2765d4691a160d5ab9b6be06
Roydon Jeffrey <brushdam14823@gmail.com>
2016-08-02 16:59:42 -0400

    Successfully implemented the logics to determine a winner but not yet completed the draw/tie state of the game

--- tic-tac-toe/ViewController.swift
+++ tic-tac-toe/ViewController.swift
@@ -17,9 +17,9 @@ class ViewController: UIViewController {
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     //To determine when a winner should be labeled
-    let winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
-                         [1, 4, 7], [2, 5, 8], [3, 6, 9],
-                         [1, 5, 9], [3, 5, 7]
+    let winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                         [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                         [0, 4, 8], [2, 4, 6]
                         ]
     
     @IBOutlet var resultsLabel: UILabel!
@@ -42,6 +42,18 @@ class ViewController: UIViewController {
             }else {
                 sender.setImage(UIImage(named: "tic-tac-toe-o copy"), forState: .Normal)
                 currentPlayer = 1
+            }
+            
+            for comboSet in winningCombos {
+                if gameState[comboSet[0]] != 0 {
+                    if ((gameState[comboSet[0]] == gameState[comboSet[1]]) && (gameState[comboSet[1]] == gameState[comboSet[2]])) {
+                        if gameState[comboSet[0]] == 1 {
+                            resultsLabel.text = "X Wins!!"
+                        }else {
+                            resultsLabel.text = "O Wins!!"
+                        }
+                    }
+                }
             }
         }
     }
