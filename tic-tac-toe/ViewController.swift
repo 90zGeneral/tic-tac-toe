939e6c91d9c05856082aab2cf8d4b0fda18d8014
Roydon Jeffrey <brushdam14823@gmail.com>
2016-08-03 17:25:25 -0400

    Added the logics for a draw game and an animation on the result label

--- tic-tac-toe/ViewController.swift
+++ tic-tac-toe/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     //To prevent any override btw x and o
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
+    var totalClick = 0
+    
     //To determine when a winner should be labeled
     let winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                          [0, 3, 6], [1, 4, 7], [2, 5, 8],
@@ -52,8 +54,20 @@ class ViewController: UIViewController {
                         }else {
                             resultsLabel.text = "O Wins!!"
                         }
+                        
+                        //To Animate the label back onto the screen
+                        resultsLabel.hidden = false
+                        UIView.animateWithDuration(0.5, animations: {
+                            self.resultsLabel.center = CGPointMake(self.resultsLabel.center.x + 400, self.resultsLabel.center.y)
+                        })
                     }
                 }
+            }
+            
+            totalClick += 1
+            
+            if totalClick == 9 && resultsLabel.text != "X Wins!!" && resultsLabel.text != "O Wins!!" {
+                resultsLabel.text = "DRAW!!"
             }
         }
     }
@@ -68,6 +82,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //To hide the label off the screen
+        resultsLabel.hidden = true
+        resultsLabel.center = CGPointMake(resultsLabel.center.x - 400, resultsLabel.center.y)
     }
 
     override func didReceiveMemoryWarning() {
